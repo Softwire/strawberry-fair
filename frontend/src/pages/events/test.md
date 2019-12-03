5ecadf7a80d01893a397d46c6692b8ef4115ad7b
Tim Leach <Tim.Leach@softwire.com>
2019-12-03 15:11:21 +0000

    SF-19 Added event info template

---
+++ frontend/src/pages/events/test.md
@@ -0,0 +1,22 @@
+---
+templateKey: event-info
+title: 'Parade'
+image: /img/love-a-fair.jpg
+dateTime: 2019-11-30 11:30:00
+isMeeting: false
+---
+## Join the Parade!
+
+### The Parade marks the start of Strawberry Fair, leaving from Christ's Pieces, venturing through town, around the Market Square and back to Midsummer Common in full technicolour glory.
+
+From samba to capoeira; from belly dancers to stiltwalking pirates to Lindy Hoppers, including local costume designers, school children, parents, dancers, taggers along and random members of the public, the Strawberry Fair Parade is the most colourful and exciting spectacle to grace the streets of Cambridge.
+
+## Get involved
+
+We'd love everyone and anyone to join the parade.
+
+We're also looking for local community groups involving dance, movement and colourful displays to join us in the parade and on our dedicated performance arena 'The Village Green' after the parade, so if you know of a hidden gem in Cambridge's fair city, please get in touch.
+
+If you want to get involved or have any ideas for the parade, please fill in this form or email parade@strawberry-fair.org.uk.
+
+Keep up to date with the latest news, including costume making, on the Facebook page.
---
+++ frontend/src/templates/event-info.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import { HTMLContent } from '../components/Content'
+
+
+// used by website and CMS previews
+export const EventInfoContent = ({title, dateTime, image, body, contentComponent}) => {
+    const BodyComponent = contentComponent || HTMLContent
+
+    const date = new Date(dateTime)
+
+    return (
+    <section>
+        <h1>{title}</h1>
+        <p>{date.toLocaleString('en-GB', {timeStyle: 'short'})}</p>
+        <BodyComponent content={body} />
+        <PreviewCompatibleImage imageInfo={image} />
+    </section>
+)}
+
+const EventInfo = ({data: {markdownRemark}}) => (
+    <EventInfoContent
+        title={markdownRemark.frontmatter.title}
+        dateTime={markdownRemark.frontmatter.dateTime}
+        image={markdownRemark.frontmatter.image}
+        body={markdownRemark.html}
+        isMeeting={markdownRemark.frontmatter.isMeeting}
+    />
+)
+
+export default EventInfo
+
+export const query = graphql`
+query eventInfoTemplate($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+        frontmatter {
+            title
+            dateTime
+            image {
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            isMeeting
+        }
+        html
+    }
+}
+`
