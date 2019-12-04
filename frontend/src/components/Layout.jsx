89d538d039905244f6ca169f6952d86d00af80b3
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-04 16:53:40 +0000

    sf-6: layout accepts heroImageSrc and heroImageAlt as arguments

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
 import '../styling/styles.sass'
 
 
 /* All page templates should be wrapped in the Layout component to provide common styling */
 
-export const Layout = ({children, heroImage}) => (
+export const Layout = ({children, heroImageSrc, heroImageAlt}) => (
     <div className="container">
         <Header />
         <section class="hero">
             <div class="hero-body">
                 <figure class="image">
-                    <img src="img/test-banner.jpg" alt="as-yet-untitled"></img>
+                    <PreviewCompatibleImage imageInfo={{image: {heroImageSrc}, alt: {heroImageAlt}}} />
                 </figure>
             </div>
         </section>
