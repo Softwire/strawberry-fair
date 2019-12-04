23b1a862f7f8f12986610f3c23dd1a6ab6557752
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-04 09:30:42 +0000

    SF-6 menus now working on desktop too

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,64 +2,72 @@ import React, {useState} from 'react'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 
-export const Header = () => (
-    <header>
-        <nav className="navbar is-fixed-top">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    <PreviewCompatibleImage imageInfo={logo} />
-                </a>
-                <NavBurger target="navigationBar" />
-            </div>
-            <NavMenu>
-                {[
-                    <NavDropdown title="About Us">
-                        {[
-                            <NavLink href="/" title="Frequently Asked Questions"/>,
-                            <NavLink href="/" title="About the Fair"/>,
-                            <NavLink href="/" title="Organisation"/>,
-                            <NavLink href="/" title="History"/>,
-                            <NavLink href="/" title="Environment"/>,
-                            <NavLink href="/" title="Bucket Collection Partners"/>,
-                            <NavLink href="/" title="Get Involved"/>
-                        ]}
-                    </NavDropdown>,
-                    <NavDropdown href="/" title="Areas &amp; Events">
-                        
-                    </NavDropdown>,
-                    <NavDropdown href="/" title="News">
+export const Header = () => {
+    
+    const [menuActive, setMenuState] = useState(false)
 
-                    </NavDropdown>,
-                    <NavDropdown href="/" title="Traders">
+    return (
+        <header>
+            <nav className="navbar is-fixed-top">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        <PreviewCompatibleImage imageInfo={logo} />
+                    </a>
+                    <NavBurger target="navigationBar" active={menuActive} setState={setMenuState} />
+                </div>
+                <NavMenu active={menuActive}>
+                    {[
+                        <NavDropdown title="About Us">
+                            {[
+                                <NavLink href="/" title="Frequently Asked Questions"/>,
+                                <NavLink href="/" title="About the Fair"/>,
+                                <NavLink href="/" title="Organisation"/>,
+                                <NavLink href="/" title="History"/>,
+                                <NavLink href="/" title="Environment"/>,
+                                <NavLink href="/" title="Bucket Collection Partners"/>,
+                                <NavLink href="/" title="Get Involved"/>
+                            ]}
+                        </NavDropdown>,
+                        <NavDropdown href="/" title="Areas &amp; Events">
+                            
+                        </NavDropdown>,
+                        <NavDropdown href="/" title="News">
 
-                    </NavDropdown>,
-                    <NavDropdown href="/" title="Support the Fair">
+                        </NavDropdown>,
+                        <NavDropdown href="/" title="Traders">
 
-                    </NavDropdown>,
-                    <NavDropdown href="/" title="Contact">
+                        </NavDropdown>,
+                        <NavDropdown href="/" title="Support the Fair">
 
-                    </NavDropdown>
-                ]}
-            </NavMenu>
-        </nav>
-    </header>
-)
+                        </NavDropdown>,
+                        <NavDropdown href="/" title="Contact">
+
+                        </NavDropdown>
+                    ]}
+                </NavMenu>
+            </nav>
+        </header>
+    )
+}
 
 
 const logo = { alt: "Strawberry Fair Logo", image: "img/1-line-logo.png" }
 
 
-const NavMenu = ({children}) => (
-    <ul id="navigationBar" className="navbar-menu">
-        <div className="navbar-start">
-            {children}
-        </div>
-    </ul>
-)
+const NavMenu = ({children, active}) => {
+    const getName = () => `navbar-menu ${active ? "is-active" : ""}`
+    
+    return (
+        <ul id="navigationBar" className={getName()}>
+            <div className="navbar-start">
+                {children}
+            </div>
+        </ul>
+    )
+}
 
 
-const NavBurger = ({target}) => {
-    const [active, setState] = useState(false)
+const NavBurger = ({target, active, setState}) => {
     const getName = () => `navbar-burger burger ${active ? "is-active" : ""}`
 
     return (
@@ -73,12 +81,12 @@ const NavBurger = ({target}) => {
 
 
 const NavDropdown = ({title, children}) => {
-    const [active, setState] = useState(false)
-    const getName = () => `navbar-item has-dropdown ${active ? "is-active" : ""}`
+    const [dropActive, setDropState] = useState(false)
+    const getName = () => `navbar-item has-dropdown ${dropActive ? "is-active" : ""}`
     
     return (
         <li className={getName()}>
-            <button className="navbar-link" onClick={() => setState(!active)}>
+            <button className="navbar-link" onClick={() => setDropState(!dropActive)}>
                 {title}
             </button>
             <ul className="navbar-dropdown">
