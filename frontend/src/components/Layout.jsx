6f0a8b1352bedc9d93d6b3698cf7cad06e5357b9
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-04 16:20:57 +0000

    Hero template added to layout file

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,9 +6,16 @@ import '../styling/styles.sass'
 
 /* All page templates should be wrapped in the Layout component to provide common styling */
 
-export const Layout = ({children}) => (
+export const Layout = ({children, heroImage}) => (
     <div className="container">
         <Header />
+        <section class="hero">
+            <div class="hero-body">
+                <figure class="image">
+                    <img src="img/test-banner.jpg" alt="as-yet-untitled"></img>
+                </figure>
+            </div>
+        </section>
         <main>{children}</main>
         <Footer />
     </div>
