3f4122e7678d2290f895e2e43ab86bb2e355f25a
Tim Leach <Tim.Leach@softwire.com>
2019-12-04 14:17:38 +0000

    SF-20 Added demo of 'today's date' highlighting

--- frontend/src/components/calendar/CalendarDay.jsx
+++ frontend/src/components/calendar/CalendarDay.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import PreviewCompatibleImage from '../PreviewCompatibleImage'
+//import { Link } from 'gatsby'
+//import PreviewCompatibleImage from '../PreviewCompatibleImage'
 
 // Represents a day in the calendar. Will either be empty or contain a preview of an event.
 // Later will have to add support for multiple events.
 const CalendarDay = ({dayNumber}) => (
-    <div className="column is-half-mobile is-one-quarter-tablet is-1-desktop">
-        <div className="box">
+    <div className="column is-half-mobile is-one-quarter-tablet is-2-desktop">
+        <div className={dayNumber == 5 ? "box has-background-primary has-text-white" : "box"} style={{height: "100px"}}>
             <p>{dayNumber}</p>
         </div>
     </div>
