b83766da1cb383fbd673a95b30a8e2c4bdca67c6
Robert Owen <robert.owen@softwire.com>
2019-12-05 13:32:15 +0000

    sf-8: refactor templating utilies and add comments

--- frontend/src/util/templating.js
+++ frontend/src/util/templating.js
@@ -1,22 +1,26 @@
 import { Content } from '../components/Content'
 
-// Handles data format supplied by the CMS for preview purposes
+// This function prepares a page template for use as a CMS preview component.
+// It maps props from the CMS preview format to that expected by a "normal" React component.
 export const preview = (component, placeholderProps = {}) => ({ entry, widgetFor, getAsset }) => {
-    const data_props = entry.getIn(['data']).toObject()
-    const preview_props = {}
+    const dataProps = entry.getIn(['data']).toObject()
+    const previewProps = {}
+    previewProps.content = widgetFor('body')
+    previewProps.contentComponent = Content
 
-    Object.entries(data_props)
-        .forEach(([key, value]) => preview_props[key] = (value.startsWith('/img/')) ? getAsset(value) : value);
-
-    preview_props.content = widgetFor('body')
-    preview_props.contentComponent = Content
+    // For image data, call getAsset to get the correct image object.
+    Object.entries(dataProps)
+        .forEach(([key, value]) => previewProps[key] = (
+            (typeof value === 'string' || value instanceof String) && value.startsWith('/img/')
+            ) ? getAsset(value) : value);
     
-    return component(Object.assign(placeholderProps, preview_props))
+    return component(Object.assign(placeholderProps, previewProps))
 }
 
 
-// Handles data format created by graphql
-export const site = (component, additionalPropsExtractor = data => {}) => ({data}) => {
+// This function prepares a page template for use by GatsbyJS.
+// It maps props from the graphql response format to that expected by a "normal" React component.
+export const site = (component, additionalPropsExtractor = graphqlData => {}) => ({data}) => {
     const new_props = data.markdownRemark.frontmatter
     new_props.content = data.markdownRemark.html
     return component(Object.assign(new_props, additionalPropsExtractor(data)))
