f6180bd43619d63987a9d28e99b09f8c8f49b37e
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-05 11:14:24 +0000

    sf-17: added hero CMS widget and accessible image widget template

--- frontend/src/cms/cms.js
+++ frontend/src/cms/cms.js
@@ -1,6 +1,7 @@
 import CMS from 'netlify-cms-app'
 
 import '../styling/styles.sass'
+<<<<<<< HEAD
 import { HomePage } from '../templates/home-page'
 import { NewsOverview } from '../templates/news-overview'
 import { preview } from '../util/templating'
@@ -27,3 +28,10 @@ CMS.registerPreviewTemplate('home', preview(HomePage))
 CMS.registerPreviewTemplate('events', preview(EventInfo))
 CMS.registerPreviewTemplate('news', preview(NewsArticle))
 CMS.registerPreviewTemplate('news-home', preview(NewsOverview, {newsArticles: placeholderArticles}))
+=======
+
+CMS.registerPreviewTemplate('home', HomePagePreview)
+CMS.registerPreviewTemplate('events', EventInfoPreview)
+CMS.registerPreviewTemplate('news', NewsArticlePreview)
+CMS.registerPreviewTemplate('news-home', NewsOverviewPreview)
+>>>>>>> sf-17: added hero CMS widget and accessible image widget template
