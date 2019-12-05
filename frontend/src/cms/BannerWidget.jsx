0bf5536257cfa158647448d5f85670290d356f73
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-05 09:22:46 +0000

    sf-17: added custom widget structure

---
+++ frontend/src/cms/BannerWidget.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const BannerWidgetControl = ({}) => {
+
+    const handleChange = () => (
+
+    )
+
+    return (
+
+    )
+}
+
+export const BannerWidgetPreview = null
+
+export const BannerWidgetEditor = null
--- frontend/src/cms/cms.js
+++ frontend/src/cms/cms.js
@@ -6,8 +6,12 @@ import NewsArticlePreview from './preview-templates/NewsArticlePreview'
 import NewsOverviewPreview from './preview-templates/NewsOverviewPreview'
 
 import '../styling/styles.sass'
+import { BannerWidgetControl, BannerWidgetPreview, BannerWidgetEditor } from './BannerWidget'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('events', EventInfoPreview)
 CMS.registerPreviewTemplate('news', NewsArticlePreview)
 CMS.registerPreviewTemplate('news-home', NewsOverviewPreview)
+
+CMS.registerWidget("BannerImages", BannerWidgetControl, BannerWidgetPreview)
+CMS.registerEditorComponent(BannerWidgetEditor)
