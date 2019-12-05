956cd4600d7a8a60f4dae68f599a81f6b4e8ecd8
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-05 16:24:03 +0000

    sf-17: banner now functional

--- frontend/src/templates/home-page.jsx
+++ frontend/src/templates/home-page.jsx
@@ -28,6 +28,7 @@ export const HomePage = ({title, content, image, contentComponent}) => {
     </section>
 )}
 
+<<<<<<< HEAD
 const HomePage = ({data: {markdownRemark}}) => (
   <Layout hero={markdownRemark.frontmatter.hero}>
     <HomePageContent
@@ -38,6 +39,43 @@ const HomePage = ({data: {markdownRemark}}) => (
   </Layout>
 )
 >>>>>>> sf-17: written Hero component
+=======
+const HomePage = ({data: {markdownRemark}}) => {
+
+  const hero = ({
+    image1: {
+      alt: markdownRemark.frontmatter.hero.image1.alt,
+      src: markdownRemark.frontmatter.hero.image1.src
+    },
+    image2: {
+      alt: markdownRemark.frontmatter.hero.image2.alt,
+      src: markdownRemark.frontmatter.hero.image2.src
+    },
+    image3: {
+      alt: markdownRemark.frontmatter.hero.image3.alt,
+      src: markdownRemark.frontmatter.hero.image3.src
+    },
+    image4: {
+      alt: markdownRemark.frontmatter.hero.image4.alt,
+      src: markdownRemark.frontmatter.hero.image4.src
+    },
+    image5: {
+      alt: markdownRemark.frontmatter.hero.image5.alt,
+      src: markdownRemark.frontmatter.hero.image5.src
+    }
+  })
+  
+  return (
+    <Layout hero={hero}>
+      <HomePageContent
+          title={markdownRemark.frontmatter.title}
+          content={markdownRemark.html}
+          image={markdownRemark.frontmatter.image}
+      />
+    </Layout>
+  )
+}
+>>>>>>> sf-17: banner now functional
 
 export default site(HomePage)
 
@@ -46,10 +84,55 @@ query homePageTemplate($id: String!) {
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
+        hero {
+          image1 {
+            alt
+            src {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image2 {
+            alt
+            src {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image3 {
+            alt
+            src {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image4 {
+            alt
+            src {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image5 {
+            alt
+            src {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
             }
           }
         }
@@ -58,3 +141,4 @@ query homePageTemplate($id: String!) {
     }
   }
 `
+
