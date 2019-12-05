71dfc84b7bab79c4563928bc19c17f63fc393bfa
Tim Leach <Tim.Leach@softwire.com>
2019-12-05 14:46:10 +0000

    SF-20 Implemented 'Upcoming events' page

--- frontend/src/components/calendar/Upcoming.jsx
+++ frontend/src/components/calendar/Upcoming.jsx
@@ -1,3 +1,29 @@
 import React from 'react'
 
+// TODO: Filter meetings/non-meetings
+const Upcoming = ({events}) => {
+    const maxItems = 10
+
+    // Construct array of list elements
+    let eventPanels = []
+
+    for (let i = 0; i < maxItems && i < events.length; i++) {
+        const event = events[i].node
+        eventPanels.push(<EventPanelBlock key={i} event={event} />)
+    }
+
+    return (
+        <div className="panel">
+            <p className="panel-heading">Upcoming</p>
+            {eventPanels}
+        </div>
+    )
+}
+
+const EventPanelBlock = ({event}) => {
+    return (
+        <p className="panel-block">{event.frontmatter.title}</p>
+    )
+}
+
 export default Upcoming
--- frontend/src/templates/calendar-page.jsx
+++ frontend/src/templates/calendar-page.jsx
@@ -3,46 +3,86 @@ import { graphql } from 'gatsby'
 
 import { Layout } from '../components/Layout'
 import Calendar from '../components/calendar/Calendar'
+import Upcoming from '../components/calendar/Upcoming'
 import { FaCalendar, FaListUl } from 'react-icons/fa'
 
-export const CalendarPageContent = ({title, focusDate, events}) => {
+class CalendarPageContent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: 'calendar',
+      title: props.title,
+      focusDate: props.focusDate,
+      events: props.events
+    }
+    this.toUpcoming = this.toUpcoming.bind(this)
+    this.toCalendar = this.toCalendar.bind(this)
+  }
+
+  toUpcoming() {
+    this.setState({view: 'upcoming'})
+  }
+
+  toCalendar() {
+    this.setState({view: 'calendar'})
+  }
+
+  render() {
+    const title = this.state.title
+    const focusDate = this.state.focusDate
+    const events = this.state.events
+
+    let inner;
+
+    if (this.state.view === 'calendar') {
+      inner = (
+        <Calendar title={title} focusDate={focusDate} events={events} />
+      )
+    } else if (this.state.view === 'upcoming') {
+      inner = (
+        <Upcoming events={events} />
+      )
+    }
+
     return (
-    <section>
-      <div className="tabs is-centered">
-        <ul>
-          <li className="is-active"><a href="/calendar">
-            <span className="icon is-small"><FaCalendar /></span>Calendar
-          </a></li>
-          <li><a href="/calendar">
-            <span className="icon is-small"><FaListUl /></span>Upcoming events
-          </a></li>
-        </ul>
-      </div>
-      <Calendar title={title} focusDate={focusDate} events={events} />
-    </section>
-)}
+      <section>
+        <div className="tabs is-centered is-boxed">
+          <ul>
+            <li className={this.state.view === 'calendar' ? 'is-active' : ''}><a onClick={this.toCalendar}>
+              <span className="icon is-small"><FaCalendar /></span>Calendar
+            </a></li>
+            <li className={this.state.view === 'upcoming' ? 'is-active' : ''}><a onClick={this.toUpcoming}>
+              <span className="icon is-small"><FaListUl /></span>Upcoming events
+            </a></li>
+          </ul>
+        </div>
+        {inner}
+      </section>
+    )
+  }
+}
 
 const CalendarPage = ({data: {markdownRemark, allMarkdownRemark}}) => {
-    const layout = false
+  const layout = false
 
-    if (layout) {
-      return (
-      <Layout>
-          <CalendarPageContent
-              title={markdownRemark.frontmatter.title}
-              focusDate={new Date()}
-              events={allMarkdownRemark.edges}
-          />
-      </Layout>)
-    } else {
-      return (
+  if (layout) {
+    return (
+    <Layout>
         <CalendarPageContent
             title={markdownRemark.frontmatter.title}
             focusDate={new Date()}
             events={allMarkdownRemark.edges}
         />
-      )
-    }
+    </Layout>)
+  } else {
+    return (
+      <CalendarPageContent
+          title={markdownRemark.frontmatter.title}
+          focusDate={new Date()}
+          events={allMarkdownRemark.edges}
+      />
+    )
+  }
 }
 
 export default CalendarPage
