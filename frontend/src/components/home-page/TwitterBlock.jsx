c797625d6d658db369a65fa78aed9320f7d85c0c
Joel Wee <Joel.Wee@softwire.com>
2019-12-09 13:59:08 +0000

    SF-3: Resize twitter width

--- frontend/src/components/home-page/TwitterBlock.jsx
+++ frontend/src/components/home-page/TwitterBlock.jsx
@@ -5,12 +5,16 @@ import BaseBlock from './BaseBlock'
 
 const TwitterBlock = ({twitterBlock}) => (
   <BaseBlock block={twitterBlock}>
-    <div className="container">
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName="strawberry_fair"
-        options={{height: 800}}
-      />
+    <div className="columns is-centered">
+      <div className="column is-9">
+        <div className="box">
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="strawberry_fair"
+            options={{height: 800}}
+          />
+        </div>
+      </div>
     </div>
   </BaseBlock>
 )
