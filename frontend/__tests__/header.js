2e5670680127d71f5f21402231aac949018632bc
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-09 16:03:57 +0000

    sf-24: first unit tests added

--- frontend/__tests__/header.js
+++ frontend/__tests__/header.js
@@ -1,13 +1,19 @@
 import React from "react"
-import renderer from "react-test-renderer"
+
 
 import { Header } from "../src/components/Header"
+import { render, fireEvent } from "@testing-library/react"
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+test("Dropdown menus closed on loading", () => {
+  const { getByText } = render(<Header />)
+
+  expect(getByText("Frequently Asked Questions")).not.toBeVisible()
+})
+
+test("Dropdown menus can open", async () => {
+  const { getByText } = render(<Header />)
+
+  fireEvent.click(getByText("About Us"))
+
+  await expect(getByText("Frequently Asked Questions")).toBeVisible()
 })
