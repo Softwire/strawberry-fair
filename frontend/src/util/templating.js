63f0c0b06f00e6dba0093d6871c6490c7d4f56aa
Joel Wee <Joel.Wee@softwire.com>
2019-12-09 13:59:56 +0000

    SF-3: Add docs for templating.js

--- frontend/src/util/templating.js
+++ frontend/src/util/templating.js
@@ -1,21 +1,44 @@
 import { Content } from '../components/Content'
 
-// This function prepares a page template for use as a CMS preview component.
-// It maps props from the CMS preview format to that expected by a "normal" React component.
-export const preview = (component, placeholderProps = {}, additionalPropsExtractor = (dataProps, helperFunctions) => {}) => ({ entry, widgetFor, widgetsFor, getAsset }) => {
-    const dataProps = entry.getIn(['data']).toJS()
-    const previewProps = {}
-    previewProps.content = widgetFor('body')
-    previewProps.contentComponent = Content
-    Object.assign(previewProps, additionalPropsExtractor(dataProps, { widgetsFor }))
+/**
+ * This function prepares a page template for use as a CMS preview component.
+ * It maps props from the CMS preview format to that expected by a "normal" React component.
+ * @param {Object} component - Template component to preview
+ * @param {Object} placeholderProps - For insertion of props that are unavailable in the CMS
+ * @param {Function} additionalPropsExtractor - To extract props that are unavailable in the CMS, e.g. props living in `node.fields`
+ * @param {Object} dataProps - The data read from the CMS
+ * @param {Object} cmsUtilityFns - Utility functions provided by the CMS
+ * @returns {Function} Function to be passed into the CMS.registerPreviewTemplate(...) function
+ */
+export const preview = (component, placeholderProps = {}, additionalPropsExtractor = (dataProps, cmsUtilityFns) => {}) => {
+    /**
+     * @param {Object} entry.data - The data read from the CMS in Immutable.js object
+     * @param {Function} widgetFor - Utility function provided by the CMS
+     * @param {Function} widgetsFor - Utility function provided by the CMS
+     * @param {Function} getAsset - Utility function provided by the CMS
+     */
+    return ({ entry, widgetFor, widgetsFor, getAsset }) => {
+        const dataProps = entry.getIn(['data']).toJS()
+        const previewProps = {}
 
-    // For image data, call getAsset to get the correct image object.
-    deepReplaceImageUrlsWithAssets(dataProps, getAsset)
-    Object.assign(previewProps, dataProps)
+        previewProps.content = widgetFor('body')
+        previewProps.contentComponent = Content
 
-    return component(Object.assign(placeholderProps, previewProps))
+        Object.assign(previewProps, additionalPropsExtractor(dataProps, { widgetsFor }))
+
+        // For image data, call getAsset to get the correct image object.
+        deepReplaceImageUrlsWithAssets(dataProps, getAsset)
+        Object.assign(previewProps, dataProps)
+
+        return component(Object.assign(placeholderProps, previewProps))
+    }
 }
 
+/**
+ * Replace image urls with assets recursively for the object passed in
+ * @param {Object} obj 
+ * @param {Function} getAsset - Utility function provided by NetlifyCMS
+ */
 const deepReplaceImageUrlsWithAssets = (obj, getAsset) => {
     Object.entries(obj)
         .forEach(([key, value]) => {
@@ -27,10 +50,21 @@ const deepReplaceImageUrlsWithAssets = (obj, getAsset) => {
         })
 }
 
-// This function prepares a page template for use by GatsbyJS.
-// It maps props from the graphql response format to that expected by a "normal" React component.
-export const site = (component, additionalPropsExtractor = graphqlData => {}) => ({data}) => {
-    const new_props = data.markdownRemark.frontmatter
-    new_props.content = data.markdownRemark.html
-    return component(Object.assign(new_props, additionalPropsExtractor(data)))
+/**
+ * This function prepares a page template for use by GatsbyJS.
+ * It maps props from the graphql response format to that expected by a "normal" React component.
+ * @param {Object} component - Component to preview, e.g. "home-page"
+ * @param {Function} additionalPropsExtractor - To extract props that are unavailable in the CMS, e.g. props living in `node.fields`
+ * @returns {Function} Function that renders the component with the data passed in by Graphql, according to Gatsby convention
+ */
+export const site = (component, additionalPropsExtractor = graphqlData => {}) => {
+    /**
+     * @param {Object} data - Data retrieved from GraphQL query specified in the component
+     * @returns {React.Component} Component to be rendered by Gatsby
+     */
+    return ({data}) => {
+        const new_props = data.markdownRemark.frontmatter
+        new_props.content = data.markdownRemark.html
+        return component(Object.assign(new_props, additionalPropsExtractor(data)))
+    }
 }
