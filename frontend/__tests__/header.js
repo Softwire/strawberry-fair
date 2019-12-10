8ab1d92485978f8945287dbbac6b76db504475ce
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-12-10 17:17:36 +0000

    sf-24: added new unit test

--- frontend/__tests__/header.js
+++ frontend/__tests__/header.js
@@ -4,10 +4,10 @@ import React from "react"
 import { Header } from "../src/components/Header"
 import { render, fireEvent } from "@testing-library/react"
 
-test("Dropdown menus closed on loading", () => {
+test("Dropdown menus closed on loading", async () => {
   const { getByText } = render(<Header />)
 
-  expect(getByText("Frequently Asked Questions")).not.toBeVisible()
+  await expect(getByText("Frequently Asked Questions")).not.toBeVisible()
 })
 
 test("Dropdown menus can open", async () => {
@@ -16,4 +16,13 @@ test("Dropdown menus can open", async () => {
   fireEvent.click(getByText("About Us"))
 
   await expect(getByText("Frequently Asked Questions")).toBeVisible()
+})
+
+test("Dropdown menus close when other menus clicked", async () => {
+  const { getByText } = render(<Header />)
+
+  fireEvent.click(getByText("About Us"))
+  fireEvent.click(getByText("Organisation"))
+
+  await expect(getByText("Frequently Asked Questions")).not.toBeVisible()
 })
