05465c179379752ffa28bb597f4ec6b54ba85a90
Tim Leach <Tim.Leach@softwire.com>
2019-12-10 10:30:35 +0000

    SF-26 Fixed code review comments

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -5,14 +5,14 @@ export const HTMLContent = ({ content, className }) => (
     <div className={className + " content"} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
-// This component is here to make sure the HTML in a preview (e.g. upcoming events) isn't too large.
+// This component is here to make sure the HTML in a smaller frame (e.g. upcoming events) isn't too large.
 // It basically renders <h1>s and <h2>s etc as <h4>s <h5>s etc.
-export const HTMLContentPreview = ({ content, className }) => (
+export const HTMLContentSmall = ({ content, className }) => (
     <div className={className + " content is-small"} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
 export const Content = ({ content, className }) => (
-    <div className={className}>{content}</div>
+    <div className={className + " content"}>{content}</div>
 )
 
 Content.propTypes = {
