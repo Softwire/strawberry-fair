14f708d4bf90f7ac65a8767017dd598e97577662
Tim Leach <Tim.Leach@softwire.com>
2019-12-10 17:39:57 +0000

    SF-47 Can now filter by tag on the upcoming events page

---
+++ frontend/src/components/calendar/Event.jsx
@@ -0,0 +1,8 @@
+// List of allowed event tags.
+// This should always be identical to the list in static/admin/config.yml
+export const eventTagList = [
+    'Benefit gig',
+    'Meeting - Programming',
+    'Meeting - Finance',
+    'Band competition'
+]
--- frontend/src/components/calendar/Upcoming.jsx
+++ frontend/src/components/calendar/Upcoming.jsx
@@ -1,27 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
 import { HTMLContentSmall } from '../Content'
-
-// TODO: Filter meetings/non-meetings
-export const Upcoming = ({events}) => {
-    const maxItems = 10
-
-    // Construct array of list elements
-    let eventPanels = []
-
-    for (let i = 0; i < maxItems && i < events.length; i++) {
-        const event = events[i].node
-        eventPanels.push(<EventPanelBlock key={i} event={event} />)
-    }
-
-    return (
-        <div className="panel">
-            <h2 className="panel-heading">Upcoming</h2>
-            {eventPanels}
-        </div>
-    )
-}
+import { eventTagList } from './Event'
 
 const EventPanelBlock = ({event}) => {
     return (
@@ -39,6 +21,85 @@ const EventPanelBlock = ({event}) => {
                     <HTMLContentSmall content={event.html} />
                 </div>
             </div>
+        </div>
+    )
+}
+
+const EnabledFilterTag = ({name, remove}) => (
+    <a className="tag is-primary is-light" onClick={remove}>{name}</a>  // Due to this weird syntax, addFilter below must be a function that takes a string and returns a function that adds that filter. How very Haskell of me!
+)
+
+EnabledFilterTag.propTypes = {
+    name: PropTypes.string,
+    remove: PropTypes.func
+}
+
+const DisabledFilterTag = ({name, add}) => (
+    <a className="tag" onClick={add}>{name}</a>
+)
+
+DisabledFilterTag.propTypes = {
+    name: PropTypes.string,
+    add: PropTypes.func
+}
+
+const UpcomingFilterBlock = ({allFilters, activeFilters, addFilter, removeFilter}) => {
+    // Construct array of tag objects
+    let tags = []
+    for (const filter of allFilters) {
+        if (activeFilters.includes(filter)) {
+            tags.push(<EnabledFilterTag key={filter} name={filter} remove={removeFilter(filter)} />)
+        } else {
+            tags.push(<DisabledFilterTag key={filter} name={filter} add={addFilter(filter)} />)
+        }
+    }
+
+    return (
+        <div className="panel-block">
+            <div className="tags">
+                {tags}
+            </div>
+        </div>
+    )
+}
+
+UpcomingFilterBlock.propTypes = {
+    allFilters: PropTypes.arrayOf(PropTypes.string),
+    activeFilters: PropTypes.arrayOf(PropTypes.string),
+    addFilter: PropTypes.func.isRequired,
+    removeFilter: PropTypes.func.isRequired
+}
+
+export const Upcoming = ({events}) => {
+    const [filters, setFilters] = useState([])  // Filter events by tags
+
+    const addFilter = (filterName) => (
+        () => {setFilters(filters.concat(filterName))}  // Gotta love functional programming
+    )
+
+    const removeFilter = (filterName) => (
+        () => {setFilters(filters.filter(name => name !== filterName))}  // Set 'filters' to the existing 'filters' array, filtered (confusing) to contain only the elements not matching the given name
+    )
+
+    const maxItems = 10
+
+    // Construct array of list elements
+    let eventPanels = []
+
+    for (let i = 0; i < maxItems && i < events.length; i++) {
+        const event = events[i].node
+
+        // Does this event have a tag that's included in the filter?
+        if (filters.length == 0 || event.frontmatter.tags.some(tag => filters.includes(tag))) {  // i.e. if there are no filters OR this event has a tag we're filtering for
+            eventPanels.push(<EventPanelBlock key={i} event={event} />)
+        }
+    }
+
+    return (
+        <div className="panel">
+            <h2 className="panel-heading">Upcoming</h2>
+            <UpcomingFilterBlock allFilters={eventTagList} activeFilters={filters} addFilter={addFilter} removeFilter={removeFilter} />
+            {eventPanels}
         </div>
     )
 }
