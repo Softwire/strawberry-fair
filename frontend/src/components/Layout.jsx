34617df3030d33653e53d136fc01277f9c0c8890
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-12 18:44:46 +0000

    SF-60: add links to the header buttons.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import { Header } from './Header'
 import { Footer } from './Footer'
 import '../styling/styles.scss'
@@ -27,3 +26,4 @@ Layout.propTypes = {
     children: PropTypes.node,
     heroData: Header.propTypes.heroData
 }
+
