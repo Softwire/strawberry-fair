efa0e23b879874555d76d7633bc4bdb83974a642
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-13 09:50:02 +0000

    SF-60: Move the buttons to their own component.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,43 +3,21 @@ import PropTypes from 'prop-types'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { useStaticQuery, graphql, Link } from 'gatsby'
+import HeaderButtons from './headerButtons'
 import '../styling/styles.scss'
 
 
 /* All page templates should be wrapped in the Layout component to provide common styling */
 
-export const Layout = ({children, revolvingHero, fixedHero}) => {
-    const data = useStaticQuery(graphql`
-    query headerButtons {
-            applyToTrade: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/apply-to-trade.md/"}) {
-              frontmatter {
-                link
-                title
-              }
-            }
-            getInvolved: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/get-involved.md/"}) {
-                frontmatter {
-                  link
-                  title
-                }
-              }
-          }`
-        )
-    return(
+export const Layout = ({children, revolvingHero, fixedHero}) => (
         <div id = "root-layout" className="container">
             <Header revolvingHero={revolvingHero} fixedHero={fixedHero}>
-                <div className="field is-grouped is-pulled-right">
-                    <div className="buttons">
-                        <Link to={data.getInvolved.frontmatter.link} className="button is-secondary">{data.getInvolved.frontmatter.title}</Link>
-                        <Link to={data.applyToTrade.frontmatter.link} className="button is-primary">{data.applyToTrade.frontmatter.title}</Link>
-                    </div>
-                </div>
+                <HeaderButtons/>
             </Header>
             <main>{children}</main>
             <Footer />
         </div>
         )
-    }
 
 
 Layout.propTypes = {
