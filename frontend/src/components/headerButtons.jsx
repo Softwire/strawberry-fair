8dd8422a12e182ee23fc248c54f0d94a5ff5bc6b
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-16 10:21:09 +0000

    SF-60: Try to fix the CMS preview breaking issue.

--- frontend/src/components/headerButtons.jsx
+++ frontend/src/components/headerButtons.jsx
@@ -1,31 +1,45 @@
 import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
+import { PreviewContext } from '../util/context.jsx'
+import headerButtonPreviewData from '../data/headerButtonPreviewData'
 
-const HeaderButtons = () => {
-    const data = useStaticQuery(graphql`
-    query headerButtons {
-            applyToTrade: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/apply-to-trade.md/"}) {
-              frontmatter {
-                link
-                title
-              }
-            }
-            getInvolved: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/get-involved.md/"}) {
-                frontmatter {
-                  link
-                  title
-                }
-              }
-          }`
-        )
+const HeaderButtons = () => (
+  <PreviewContext.Consumer>
+    {value => <HeaderButtonsDisplay isPreview={value} />}
+  </PreviewContext.Consumer>
+)
+
+export default HeaderButtons
+
+const HeaderButtonsDisplay = ({isPreview}) => {
+  const data = isPreview ? headerButtonPreviewData : getHeaderButtonLinksAndText()
+
+    
     return(
             <div className="field is-grouped is-pulled-right">
                 <div className="buttons">
-                    <Link to={data.getInvolved.frontmatter.link} className="button is-secondary">{data.getInvolved.frontmatter.title}</Link>
-                    <Link to={data.applyToTrade.frontmatter.link} className="button is-primary">{data.applyToTrade.frontmatter.title}</Link>
+                    <Link to={data.getInvolved.frontmatter.link} className="button is-secondary">{data.getInvolved.frontmatter.text}</Link>
+                    <Link to={data.applyToTrade.frontmatter.link} className="button is-primary">{data.applyToTrade.frontmatter.text}</Link>
                 </div>
             </div>
     )
 }
 
-export default HeaderButtons
+function getHeaderButtonLinksAndText() {
+  return useStaticQuery(graphql`
+    query headerButtons {
+            applyToTrade: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/apply-to-trade.md/"}) {
+              frontmatter {
+                link
+                text
+              }
+            }
+            getInvolved: markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/buttons/get-involved.md/"}) {
+                frontmatter {
+                  link
+                  text
+                }
+              }
+          }`
+        )
+}
