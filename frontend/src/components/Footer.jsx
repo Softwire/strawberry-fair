4244d4eed3bf6b47b47a15788a4d14b3bc2861cc
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-18 14:15:25 +0000

    SF-50: update footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { IconContext } from 'react-icons'
 import { FaFacebook, FaTwitter } from 'react-icons/fa'
-
-import { Link } from "gatsby"
-
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import footerPreviewContent from '../data/footerPreviewContent'
+import { PreviewContext } from '../util/context.jsx'
 
 export const Footer = () => (
+    <PreviewContext.Consumer>
+      {value => <FooterDisplay isPreview={value} />}
+    </PreviewContext.Consumer>
+  )
+  
+
+const FooterDisplay = ({isPreview}) => {
+    const footerContent = isPreview ? footerPreviewContent : getFooterContent()
+    return(
     <footer className="footer">
         <div className="tile is-ancestor">
             <div className="tile is-parent">
@@ -13,6 +22,7 @@ export const Footer = () => (
                     <h3 className="title">
                         Placeholder
                     </h3>
+                    {footerContent.markdownRemark.frontmatter.placeHolderText}
                 </div>
             </div>
             <div className="tile is-parent">
@@ -21,12 +31,12 @@ export const Footer = () => (
                         Follow
                     </h3>
                     <div className="tile is-parent">
-                        <a className="icon is-large facebook-colour" href="https://www.facebook.com/strawberryfair">
+                        <a className="icon is-large facebook-colour" href={footerContent.markdownRemark.frontmatter.facebookAccount}>
                             <IconContext.Provider value={{size: "2em"}}>
                                 <FaFacebook />
                             </IconContext.Provider>
                         </a>
-                        <a className="icon is-large twitter-colour" href="https://twitter.com/strawberry_fair">
+                        <a className="icon is-large twitter-colour" href={footerContent.markdownRemark.frontmatter.twitterAccount}>
                             <IconContext.Provider value={{size: "2em"}}>
                                 <FaTwitter />
                             </IconContext.Provider>
@@ -39,16 +49,35 @@ export const Footer = () => (
                     <h3 className="title">
                         Contact
                     </h3>
-                    PO Box 1261 <br /> Cambridge <br /> CB1 0YJ <br /><br /><a href="mailto:enquiries@strawberry-fair.org.uk">enquiries@strawberry-fair.org.uk</a>
+                    {footerContent.markdownRemark.frontmatter.address}<a href={"mailto:" + footerContent.markdownRemark.frontmatter.email}>{footerContent.markdownRemark.frontmatter.email}</a>
                 </div>
             </div>
         </div>
         <div className="tile is-ancestor">
             <div className="tile is-parent">
                 <div className="tile is-child box content">
-                    © Strawberry Fair 2017 | <Link to="/privacy"> Cookie & Privacy policies</Link>
+                {footerContent.markdownRemark.frontmatter.copyright} | <Link to="/privacy"> Cookie & Privacy policies</Link>
                 </div>
             </div>
         </div>
     </footer>
-)
+    )
+}
+
+function getFooterContent() {
+    return useStaticQuery(graphql`
+      query footerContent {
+              markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/footer.md/"}) {
+                frontmatter {
+                  email
+                  address
+                  facebookAccount
+                  twitterAccount
+                  placeHolderText
+                  copyright
+                }
+              }
+            }`
+          )
+}
+
