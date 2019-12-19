2d0d06a515d3cda76f553103974a579460605f7a
Tim Leach <Tim.Leach@softwire.com>
2019-12-19 12:05:40 +0000

    SF-57 Implemented outside clicking and more expected behaviour for touchscreen devices

--- frontend/src/components/header/NavBar.jsx
+++ frontend/src/components/header/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import OutsideClickHandler from 'react-outside-click-handler'
 import { graphql, Link, useStaticQuery } from 'gatsby'
 
 import PreviewCompatibleImage from '../PreviewCompatibleImage'
@@ -34,17 +35,27 @@ const NavBarDisplay = ({isPreview}) => {
   const [menuActive, setMenuState] = useState(false)
   const navBarLinks = isPreview ? navBarPreviewLinks : getNavBarLinksFromGraphqlData()
 
+  // List holding which dropdowns are open (on mobile)
+  const [dropdownsActive, setDropdownsActive] = useState(Array.from(navBarLinks).fill(false))
+
+  // Function to collapse all child dropdowns
+  const collapseAll = () => {
+    setDropdownsActive(navBarLinks.fill(false))
+  }
+
   return (
       <header>
-        <nav className="navbar">
-          <div className="navbar-brand">
-            <Link className="navbar-item" to="/">
-              <PreviewCompatibleImage imageInfo={{ alt: "Strawberry Fair Logo", image: "/img/1-line-logo.png" }} />
-            </Link>
-            <NavBurger target="navigationBar" active={menuActive} setState={setMenuState} />
-          </div>
-          <NavMenu active={menuActive} navBarLinks={navBarLinks}/>
-        </nav>
+        <OutsideClickHandler onOutsideClick={() => {collapseAll(); setMenuState(false)}} display="contents">
+          <nav className="navbar">
+            <div className="navbar-brand">
+              <Link className="navbar-item" to="/">
+                <PreviewCompatibleImage imageInfo={{ alt: "Strawberry Fair Logo", image: "/img/1-line-logo.png" }} />
+              </Link>
+              <NavBurger target="navigationBar" active={menuActive} setState={setMenuState} collapseAll={collapseAll} />
+            </div>
+            <NavMenu active={menuActive} navBarLinks={navBarLinks} collapseAll={collapseAll} dropdownsActive={dropdownsActive} setDropdownsActive={setDropdownsActive} />
+          </nav>
+        </OutsideClickHandler>
       </header>
   )
 }
@@ -97,8 +108,14 @@ const generateLinks = (navBarTabs, titleToLinkMap, graphqlEdges) => {
     }).filter(link => link !== undefined)
 }
 
-const NavBurger = ({target, active, setState}) => (
-  <a className={getClassName("navbar-burger burger","is-active", active)} data-target={target} onClick={() => setState(!active)}>
+const NavBurger = ({target, active, setState, collapseAll}) => (
+  <a className={getClassName("navbar-burger burger","is-active", active)} data-target={target} onClick={() => {
+    if (active) {
+      // If we are closing the navbar, collapse all the dropdowns
+      collapseAll()
+    }
+    setState(!active)
+  }}>
     <span></span>
     <span></span>
     <span></span>
@@ -107,28 +124,36 @@ const NavBurger = ({target, active, setState}) => (
 
 const getClassName = (baseName, toggleName, active) => `${baseName} ${active ? toggleName : ""}`
 
-const NavMenu = ({active, navBarLinks}) => (
-  <div id="navigationBar" className={getClassName("navbar-menu", "is-active", active)}>
-    <div className="navbar-start">
-      {navBarLinks.map(generateNavBarTabs)}
-    </div>
-  </div>
-)
-
-const generateNavBarTabs = (tab, tabIndex) => {
-  if(tab.noDropdown) {
-    return (
-      <NavTab link={tab.link} title={tab.title} key={tabIndex} />
-    )
-  } else {
-    return (
-      <NavDropdown 
-        title={tab.node.frontmatter.title}
-        key={tabIndex}
-        navItems={tab.node.frontmatter.pageTitles}
-      />
-    )
+const NavMenu = ({active, navBarLinks, dropdownsActive, setDropdownsActive}) => {
+  const generateNavBarTabs = (tab, tabIndex) => {
+    if(tab.noDropdown) {
+      return (
+        <NavTab link={tab.link} title={tab.title} key={tabIndex} />
+      )
+    } else {
+      return (
+        <NavDropdown 
+          title={tab.node.frontmatter.title}
+          key={tabIndex}
+          navItems={tab.node.frontmatter.pageTitles}
+          active={dropdownsActive[tabIndex]}
+          toggleFunc={() => {
+            let newDropdownsActive = Array.from(dropdownsActive)
+            newDropdownsActive[tabIndex] = !newDropdownsActive[tabIndex]
+            setDropdownsActive(newDropdownsActive)
+          }}
+        />
+      )
+    }
   }
+
+  return (
+    <div id="navigationBar" className={getClassName("navbar-menu", "is-active", active)}>
+      <div className="navbar-start">
+        {navBarLinks.map(generateNavBarTabs)}
+      </div>
+    </div>
+  )
 }
 
 const NavTab = ({title, link}) => {
@@ -139,12 +164,9 @@ const NavTab = ({title, link}) => {
   )
 }
 
-const NavDropdown = ({title, navItems}) => {
-  // Is the dropdown showing on mobile?
-  const [ active, setActive ] = useState(false)
-
+const NavDropdown = ({title, navItems, active, toggleFunc}) => {
   return (
-    <div className="navbar-item has-dropdown is-hoverable" onClick={() => setActive(!active)}>
+    <div className="navbar-item has-dropdown is-hoverable" onClick={toggleFunc}>
       <a className="navbar-link">
         {title}
       </a>
@@ -173,7 +195,9 @@ NavDropdown.propTypes = {
   navItems: PropTypes.arrayOf(PropTypes.shape({
     pageTitle: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired
-  }).isRequired).isRequired
+  }).isRequired).isRequired,
+  active: PropTypes.bool.isRequired,
+  toggleFunc: PropTypes.func.isRequired
 }
 
 NavTab.propTypes = {
@@ -198,13 +222,16 @@ NavMenu.propTypes = {
       title: PropTypes.string.isRequired
     }).isRequired
    ]).isRequired
-  )
+  ),
+  dropdownsActive: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  setDropdownsActive: PropTypes.func.isRequired
 }
 
 NavBurger.propTypes = {
   target: PropTypes.string,
   active: PropTypes.bool,
-  setState: PropTypes.func
+  setState: PropTypes.func,
+  collapseAll: PropTypes.func.isRequired
 }
 
 NavBarDisplay.propTypes = {
