99ba141123c3463382184832972097fdfa22d77c
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-19 13:33:35 +0000

    SF-60: refactoring the code

--- frontend/src/cms/LinkWidget.jsx
+++ frontend/src/cms/LinkWidget.jsx
@@ -1,41 +1,42 @@
 import React from 'react'
 import CMS from 'netlify-cms-app'
 import { Map, List } from 'immutable'
-import { getDropDownItems } from '../util/data-parser.js'
+import { getPagePathTreeObject, getSubTreeObject } from '../util/data-parser.js'
 
 // Following https://www.netlify.com/blog/2017/06/20/extending-netlify-cms-part-one-custom-widgets/
 
 const SelectionControl = CMS.getWidget('select').control
-let dropDownItems = getDropDownItems()
-const backOptionText = "<-- Previous Menu"
+let pagePathTreeObject = getPagePathTreeObject()
+const backOption = "<-- Previous Menu"
 
 export class LinkControl extends React.Component {
   constructor(props) {
     super(props)
-    this.menuPath = []
+    this.selectedOptionsHistory = []
   }
   render() {
     return (
       <div>
         <SelectionControl 
           {...this.props}
-          onChange = {val => this.handleSelectionChange(val)}
+          onChange = {selectedOption => this.handleSelectionChange(selectedOption)}
           field={this.getFields()}
         />
       </div>
       )
   }
 
-  handleSelectionChange(value) {
-      if (value === backOptionText) {
-        this.menuPath.pop()
+  handleSelectionChange(selectedOption) {
+      if (selectedOption === backOption) {
+        this.selectedOptionsHistory.pop()
+        //this makes sure that the menu field is re-rendered
         this.props.onChange("")
       }
       else {
-        const valueList = value.split("/")
-        if(valueList[valueList.length-1] !== "index") this.menuPath.push(valueList[valueList.length-1])
+        const selectedOptionWithoutPrefix = selectedOption.split("/").slice(-1)[0]
+        if(selectedOptionWithoutPrefix !== "index") this.selectedOptionsHistory.push(selectedOptionWithoutPrefix)
         //we couldn't assign value directly to this.props.value since it could contain "index". Instead we build this.props.value from the menuPath.
-        this.props.onChange("/" + this.menuPath.join("/"))
+        this.props.onChange("/" + this.selectedOptionsHistory.join("/"))
         //this line is needed to update menuPath, otherwise making the same selection twice will not call the getFields() function, since react doesn't update the component
         this.getFields()
       }
@@ -46,32 +47,19 @@ export class LinkControl extends React.Component {
   }
 
   getMenuOptionsList() {
-    let dropDownItemsList = new List(Object.keys(this.getMenuOptionsObject()))
-    const prefix = this.menuPath.length === 0 ? "/" : "/" + this.menuPath.join("/") + "/"
-    dropDownItemsList = dropDownItemsList.map(element => prefix + element)
-    return dropDownItemsList.concat(new List([backOptionText]))
+    let menuOptionsList = new List(Object.keys(this.getMenuOptionsObject()))
+    const prefix = this.selectedOptionsHistory.length === 0 ? "/" : "/" + this.selectedOptionsHistory.join("/") + "/"
+    menuOptionsList = menuOptionsList.map(element => prefix + element)
+    return menuOptionsList.concat(new List([backOption]))
   }
 
   getMenuOptionsObject() {
-    return this.menuPath.length === 0 ? dropDownItems : this.getMenuOptionsFromMenuPath(this.menuPath, dropDownItems)
-  }
-
-  getMenuOptionsFromMenuPath(menuPathAhead, currentMenuOptionsObject) {
-    if(menuPathAhead.length === 1) {
-      if(menuPathAhead[0] !== "index") {
-        const nextObject = currentMenuOptionsObject[menuPathAhead[0]]
-        const nextObjectKeys = Object.keys(nextObject)
-        if(nextObjectKeys.length === 1 && nextObjectKeys[0] === "index" ){
-          this.menuPath.pop()
-          return currentMenuOptionsObject
-        } 
-        else return currentMenuOptionsObject[menuPathAhead[0]]
-      }
-      else {
-        return this.getMenuOptionsFromMenuPath(this.menuPath.slice(0, this.menuPath.length-1), dropDownItems)
-      }
+    if(this.selectedOptionsHistory.length === 0) return pagePathTreeObject
+    else {
+    const [ menuOptionsObject, updatedOptionsHistory] = getSubTreeObject(this.selectedOptionsHistory, pagePathTreeObject, "index")
+    this.selectedOptionsHistory = updatedOptionsHistory
+    return menuOptionsObject
     }
-    else return this.getMenuOptionsFromMenuPath(menuPathAhead.slice(1), currentMenuOptionsObject[menuPathAhead[0]])
   }
 }
 
