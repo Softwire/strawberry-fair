8995298f6db39a4a2488fc56f6a74d5a19e938eb
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-20 17:25:37 +0000

    SF-60-rebase: fix lint errors.

--- frontend/src/scripts/save-page-paths.js
+++ frontend/src/scripts/save-page-paths.js
@@ -14,13 +14,18 @@ exports.savePagePathsToFile = async({ actions: graphql }) => {
         }
       }`)
       
-    if (result.errors) {
-          result.errors.forEach(e => console.error(e.toString()))
-          return Promise.reject(result.errors)
-        }
-    pagePaths = result.data.allSitePage.edges
+    if(result.errors){
+      result.errors.forEach(e=>console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
+    let pagePaths = result.data.allSitePage.edges
     pagePaths.forEach(edge => obj.pagePaths.push(edge.node.path))
     let json = JSON.stringify(obj)
-    fs.writeFile('./src/data/pagePaths.json',json,'utf-8', callback = () => {})
+    fs.writeFile('./src/data/pagePaths.json',json,'utf-8', function(error) {
+      if(error) {
+        return console.log(error)
+      }
+      console.log("pagePaths.json saved successfully.")
+    })
   }
   
