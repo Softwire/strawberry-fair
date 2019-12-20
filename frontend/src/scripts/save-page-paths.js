5a8a3e7d7298f73317b80785f2c8a6fff6f8c0f7
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-12-20 17:28:19 +0000

    SF-60-rebase: change variable declaration from let to const.

--- frontend/src/scripts/save-page-paths.js
+++ frontend/src/scripts/save-page-paths.js
@@ -18,7 +18,7 @@ exports.savePagePathsToFile = async({ actions: graphql }) => {
       result.errors.forEach(e=>console.error(e.toString()))
       return Promise.reject(result.errors)
     }
-    let pagePaths = result.data.allSitePage.edges
+    const pagePaths = result.data.allSitePage.edges
     pagePaths.forEach(edge => obj.pagePaths.push(edge.node.path))
     let json = JSON.stringify(obj)
     fs.writeFile('./src/data/pagePaths.json',json,'utf-8', function(error) {
