8124d14acc187830f7e1df5a47e010b851d852c2
Tim Leach <Tim.Leach@softwire.com>
2019-12-30 16:59:15 +0000

    SF-87 News overview now uses correct titles, and also removed unused content rendering

--- frontend/src/templates/news-overview.jsx
+++ frontend/src/templates/news-overview.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
-import { HTMLContent } from '../components/Content'
 import NewsArticleSnapshots from '../components/NewsArticleSnapshots'
 import { Layout } from '../components/Layout'
 import { site } from '../util/templating'
@@ -10,28 +9,21 @@ import NewsMenu from '../components/NewsMenu.jsx'
 
 
 // This is used by the website and for CMS previews
-export const NewsOverview = ({title, subtitle, content, contentComponent, newsArticles, heroData}) => {
-    const BodyComponent = contentComponent || HTMLContent
-    
-    return (
-      <Layout heroData={heroData}>
-        <section className="section">
-          <div className="container">
-            <h1 className="title has-text-primary is-size-1">{title}</h1>
-            <h2 className="subtitle">{subtitle}</h2>
-            <BodyComponent content={content}/>
-            <div className="columns">
-              <div className = "column is-three-quarters">
-                <NewsArticleSnapshots newsArticles={newsArticles}/>
-              </div>
-              <div className = "column">
-                <NewsMenu newsArticles={newsArticles}/>
-              </div>          
-            </div>
-          </div>
-        </section>
-      </Layout>
-)}
+export const NewsOverview = ({title, subtitle, newsArticles, heroData}) => {
+  
+  return (
+    <Layout heroData={heroData} title={title} subtitle={subtitle}>
+      <div className="columns">
+        <div className = "column is-three-quarters">
+          <NewsArticleSnapshots newsArticles={newsArticles}/>
+        </div>
+        <div className = "column">
+          <NewsMenu newsArticles={newsArticles}/>
+        </div>          
+      </div>
+    </Layout>
+  )
+}
 
 NewsOverview.propTypes = {
   title: PropTypes.node,  // Only needs to be a node, not specifically a string, as it is rendered within a tag so needs only be renderable
