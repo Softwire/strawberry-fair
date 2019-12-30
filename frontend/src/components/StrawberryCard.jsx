0f2b8d3abd7c98b4cfb840dd901d09a779064d85
Tim Leach <Tim.Leach@softwire.com>
2019-12-30 14:50:59 +0000

    SF-91 Made strawberry card use bulma 'media' object on mobile

--- frontend/src/components/StrawberryCard.jsx
+++ frontend/src/components/StrawberryCard.jsx
@@ -5,20 +5,51 @@ import VerticalTileColumn from './VerticalTileColumn'
 const defaultIcon = '/img/strawberry-icon.png'
 const defaultImageAltText = 'Strawberry Icon'
 
-export const StrawberryCard = ({image, imageAltText, text}) => (
-  <div className="box has-background-primary">
+export const StrawberryCard = ({image, imageAltText, text}) => {
+  // Card should be square on desktop (>= 770px wide) and rectangular on mobile devices (< 770px wide)
+  
+  return (
+    <div>
+      <StrawberryCardDesktop image={image} imageAltText={imageAltText} text={text} />
+      <StrawberryCardMobile  image={image} imageAltText={imageAltText} text={text} />
+    </div>
+  )
+}
+
+const StrawberryCardDesktop = ({image, imageAltText, text}) => {
+  return (
+    <div className="box has-background-primary is-hidden-mobile">
       <figure className="image is-64x64">
-          <img src={image || defaultIcon} alt={imageAltText || defaultImageAltText}/>
+        <img src={image || defaultIcon} alt={imageAltText || defaultImageAltText} />
       </figure>
       <p className="has-text-white">{text}</p>
-  </div>
-)
+    </div>
+  )
+}
+
+const StrawberryCardMobile = ({image, imageAltText, text}) => {
+  return (
+    <div className="media has-background-primary is-hidden-tablet">
+      <figure className="media-left">
+        <p className="image is-64x64">
+          <img src={image || defaultIcon} alt={imageAltText || defaultImageAltText} />
+        </p>
+      </figure>
+      <div className="media-content">
+        <p className="has-text-white">{text}</p>
+      </div>
+    </div>
+  )
+}
 
 StrawberryCard.propTypes = {
   image: PropTypes.string,
   imageAltText: PropTypes.string,
   text: PropTypes.string.isRequired
 }
+
+StrawberryCardDesktop.propTypes = StrawberryCard.propTypes
+StrawberryCardMobile.propTypes = StrawberryCard.propTypes
 
 const StrawberryTile = ({image, imageAltText, text}) => (
   <div className="tile is-child">
