5c5952c1c6d99cf4b48b8ba4f0963733428ee5a3
Tim Leach <Tim.Leach@softwire.com>
2019-12-30 16:42:42 +0000

    SF-87 Ensured titles don't render when no title or subtitle is added

--- frontend/src/components/Titles.jsx
+++ frontend/src/components/Titles.jsx
@@ -4,12 +4,16 @@ import PropTypes from 'prop-types'
 // Subtitle is rendered as <p> rather than <h2> due to instructions at
 // https://www.w3.org/TR/html52/common-idioms-without-dedicated-elements.html#subheadings-subtitles-alternative-titles-and-taglines
 export const Titles = ({title, subtitle}) => {
-  return (
-    <div className="titles">
-      <h1 className="title">{title}</h1>
-      <p className="subtitle">{subtitle}</p>
-    </div>
-  )
+  if (title || subtitle) {
+    return (
+      <div className="titles">
+        <h1 className="title">{title}</h1>
+        <p className="subtitle">{subtitle}</p>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 Titles.propTypes = {
