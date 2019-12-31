11e5e944ac6f4b22c879f3519740193346e3918f
Tim Leach <Tim.Leach@softwire.com>
2019-12-31 14:17:22 +0000

    SF-90 Hero image is now full-width

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -10,16 +10,18 @@ import '../styling/styles.scss'
 /* All page templates should be wrapped in the Layout component to provide common styling */
 
 export const Layout = ({children, heroData, title, subtitle}) => (
-    <div id="root-layout" className="section">
-        <div className="container">
-            <Header heroData={heroData}>
-                <HeaderButtons/>
-            </Header>
-            <Titles title={title} subtitle={subtitle} />
-            {children}
-            <Footer />
+    <React.Fragment>
+        <Header heroData={heroData}>
+            <HeaderButtons/>
+        </Header>
+        <div id="root-layout" className="section">
+            <div className="container">
+                <Titles title={title} subtitle={subtitle} />
+                {children}
+                <Footer />
+            </div>
         </div>
-    </div>
+    </React.Fragment>
 )
 
 Layout.propTypes = {
