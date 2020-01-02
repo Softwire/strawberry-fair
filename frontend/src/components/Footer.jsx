8b8bd00a7159a13fc523e484037dbfe8fe88752b
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-02 18:00:25 +0000

    SF-50_redone: revert changes and make footer cms configurable.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { IconContext } from 'react-icons'
 import { FaFacebook, FaTwitter } from 'react-icons/fa'
-
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import footerPreviewContent from '../data/footerPreviewContent'
+import { PreviewContext } from '../util/context.jsx'
+import { HTMLContentSmall } from './Content'
+import PropTypes from 'prop-types'
+import convertToHtml from '../util/markdown-converter.js'
 
 
 export const Footer = () => (
+    <PreviewContext.Consumer>
+      {value => <FooterDisplay isPreview={value}/>}
+    </PreviewContext.Consumer>
+  )
+
+export const FooterDisplay = ({isPreview}) => {
+
+    let footerContent = isPreview ? footerPreviewContent : getFooterContent()
+
+    footerContent.address = convertToHtml(footerContent.address)
+    footerContent.placeHolderText = convertToHtml(footerContent.placeHolderText)
+    
+    return(
     <footer className="footer">
         <div className="tile is-ancestor">
             <div className="tile is-parent">
@@ -13,6 +30,7 @@ export const Footer = () => (
                     <h3 className="title">
                         Placeholder
                     </h3>
+                    <HTMLContentSmall content={footerContent.placeHolderText}/>
                 </div>
             </div>
             <div className="tile is-parent">
@@ -21,12 +39,12 @@ export const Footer = () => (
                         Follow
                     </h3>
                     <div className="tile is-parent">
-                        <a className="icon is-large facebook-colour" href="https://www.facebook.com/strawberryfair">
+                        <a className="icon is-large facebook-colour" href={footerContent.facebookAccount}>
                             <IconContext.Provider value={{size: "2em"}}>
                                 <FaFacebook />
                             </IconContext.Provider>
                         </a>
-                        <a className="icon is-large twitter-colour" href="https://twitter.com/strawberry_fair">
+                        <a className="icon is-large twitter-colour" href={footerContent.twitterAccount}>
                             <IconContext.Provider value={{size: "2em"}}>
                                 <FaTwitter />
                             </IconContext.Provider>
@@ -39,16 +57,49 @@ export const Footer = () => (
                     <h3 className="title">
                         Contact
                     </h3>
-                    PO Box 1261 <br /> Cambridge <br /> CB1 0YJ <br /><br /><a href="mailto:enquiries@strawberry-fair.org.uk">enquiries@strawberry-fair.org.uk</a>
+                    <HTMLContentSmall content={footerContent.address}/><a href={"mailto:" + footerContent.email}>{footerContent.email}</a>
                 </div>
             </div>
         </div>
         <div className="tile is-ancestor">
             <div className="tile is-parent">
                 <div className="tile is-child box content">
-                    © Strawberry Fair 2017 | <Link to="/privacy"> Cookie & Privacy policies</Link>
+                {footerContent.copyright} | <Link to="/privacy"> Cookie & Privacy policies</Link>
                 </div>
             </div>
         </div>
     </footer>
-)
+    )
+}
+
+function getFooterContent() {
+    const footerContent = useStaticQuery(graphql`
+      query footerContent {
+              markdownRemark(fileAbsolutePath: { regex: ".*/src/pages/header-and-footer/footer.md/"}) {
+                frontmatter {
+                  email
+                  address
+                  facebookAccount
+                  twitterAccount
+                  placeHolderText
+                  copyright
+                }
+              }
+            }`
+          )
+    return {email: footerContent.markdownRemark.frontmatter.email,
+        address: footerContent.markdownRemark.frontmatter.address,
+        facebookAccount: footerContent.markdownRemark.frontmatter.facebookAccount,
+        twitterAccount: footerContent.markdownRemark.frontmatter.twitterAccount,
+        placeHolderText: footerContent.markdownRemark.frontmatter.placeHolderText,
+        copyright: footerContent.markdownRemark.frontmatter.copyright}
+}
+
+Footer.propTypes = {
+    data: PropTypes.object
+}
+
+FooterDisplay.propTypes = {
+    isPreview: PropTypes.bool.isRequired,
+    CMSInput: PropTypes.object
+}
---
+++ frontend/src/pages/header-and-footer/footer.md
@@ -0,0 +1,20 @@
+---
+email: enquiries@strawberry-fair.org.uk
+address: |-
+  PO BOX 1261
+
+  Cambridge
+
+  CB1 0YJ
+facebookAccount: 'https://www.facebook.com/StrawberryFair/'
+twitterAccount: 'https://twitter.com/strawberry_fair'
+placeHolderText: |-
+  This text
+
+  _Has **some**_
+
+  _Nice **breaks**_
+
+  and Styling
+copyright: © Strawberry Fair 2017
+---
