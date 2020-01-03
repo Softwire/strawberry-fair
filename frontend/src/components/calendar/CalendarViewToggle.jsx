8df785d3d63bdb932a67585b662a53f10888a0b1
Tim Leach <Tim.Leach@softwire.com>
2020-01-03 13:38:18 +0000

    SF-97 Improved fit of header buttons with site on mobile

--- frontend/src/components/calendar/CalendarViewToggle.jsx
+++ frontend/src/components/calendar/CalendarViewToggle.jsx
@@ -5,20 +5,24 @@ import { Link } from 'gatsby'
 import { FaCalendar, FaListUl } from 'react-icons/fa'
 
 export const CalendarViewToggle = ({view}) => (
-    <div className="tabs is-centered is-boxed">
-        <ul>
-            <li className={view == 'calendar' ? 'is-active' : ''}>
-                <Link to="/calendar">
-                <span className="icon is-small"><FaCalendar /></span>Calendar
-                </Link>
-            </li>
-            <li className={view == 'upcoming' ? 'is-active' : ''}>
-                <Link to="/events">
-                <span className="icon is-small"><FaListUl /></span>Upcoming events
-                </Link>
-            </li>
-        </ul>
-    </div>
+    <React.Fragment>
+        {['is-large is-hidden-touch', 'is-hidden-desktop'].map(modifier => (
+            <div className={`tabs is-centered is-boxed ${modifier}`} key={modifier}>
+                <ul>
+                    <li className={view == 'calendar' ? 'is-active' : ''}>
+                        <Link to="/calendar">
+                        <span className="icon is-small"><FaCalendar /></span>Calendar
+                        </Link>
+                    </li>
+                    <li className={view == 'upcoming' ? 'is-active' : ''}>
+                        <Link to="/events">
+                        <span className="icon is-small"><FaListUl /></span>Upcoming events
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        ))}
+    </React.Fragment>
 )
 
 CalendarViewToggle.propTypes = {
