691fb66a4bc87c5bd8843fefae04311af40700bb
Tim Leach <Tim.Leach@softwire.com>
2020-01-03 14:33:02 +0000

    SF-97 Calendar now has a really good-looking 'today' indicator

--- frontend/src/components/calendar/CalendarDay.jsx
+++ frontend/src/components/calendar/CalendarDay.jsx
@@ -10,8 +10,6 @@ import { areSameDay } from '../../util/dates'
 const CalendarDay = ({dateTime, events}) => {
     // TODO: Change the method of indicating it's "today", so as still to be visible when there's an event today
     const date = new Date(dateTime)  // The actual Date this CalendarDay is representing
-    const today = new Date()         // Today's date
-    const isTodayHighlight = areSameDay(date, today)
 
     // Are we showing the modal?
     const [ showModal, setShowModal ] = useState(false)
@@ -57,7 +55,7 @@ const CalendarDay = ({dateTime, events}) => {
                 <CalendarDayModal date={date} events={events} close={modalOff} active={showModal} />
                 <div className={`box button has-text-left calendar-day ${event.frontmatter.image ? "has-text-white" : "has-text-black"} has-text-weight-bold`} onClick={modalOn} style={event.frontmatter.image ? {
                         backgroundImage: `url(${event.frontmatter.image.childImageSharp ? event.frontmatter.image.childImageSharp.editedFluid.src : event.frontmatter.image})`} : null}>
-                    <p>{date.toLocaleDateString('en-GB', dateDisplayFormatOptions)}</p>
+                    <DayText date={date} />
                     <p key={event.fields.slug}><Link className={`${event.frontmatter.image ? "has-text-white" : "has-text-black"} has-text-weight-medium`} to={event.fields.slug}>{event.frontmatter.title}</Link></p>
                 </div>
             </React.Fragment>
@@ -73,7 +71,7 @@ const CalendarDay = ({dateTime, events}) => {
                             transition: `opacity ${imageFadeTimeS}s`,
                             position: "absolute",
                             width: "100%"}}>
-                        <p>{date.toLocaleDateString('en-GB', dateDisplayFormatOptions)}</p>
+                        <DayText date={date} />
                         {events.map(event => <p key={event.fields.slug}><Link className="has-text-white has-text-weight-medium" to={event.fields.slug}>{event.frontmatter.title}</Link></p>)}
                     </div>
                 )}
@@ -83,8 +81,8 @@ const CalendarDay = ({dateTime, events}) => {
         internals = (
             <React.Fragment>
                 <NoEventsModal date={date} close={modalOff} active={showModal} />
-                <div className={`box button has-text-left calendar-day ${isTodayHighlight ? "is-primary" : ""}`} onClick={modalOn}>
-                    <p>{date.toLocaleDateString('en-GB', dateDisplayFormatOptions)}</p>
+                <div className="box button has-text-left calendar-day" onClick={modalOn}>
+                    <DayText date={date} />
                 </div>
             </React.Fragment>
         )
@@ -126,6 +124,16 @@ const NoEventsModal = ({date, close, active}) => (
     </div>
 )
 
+const DayText = ({date}) => (
+    <p>
+        {areSameDay(date, new Date()) ? (
+            // This day is today, so render a pencil circle around the day text
+            <img className="pencil-circle" src="/img/pencil-circle.png" width="90" />
+        ) : null}
+        {date.toLocaleDateString('en-GB', dateDisplayFormatOptions)}
+    </p>
+)
+
 const dateDisplayFormatOptions = {weekday: 'short', day: 'numeric'}
 const longDateFormatOptions = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}
 
@@ -149,4 +157,8 @@ NoEventsModal.propTypes = {
     date: PropTypes.instanceOf(Date),
     close: PropTypes.func,
     active: PropTypes.bool
+}
+
+DayText.propTypes = {
+    date: PropTypes.instanceOf(Date)
 }
