c44fbfde552fa50f905e0b7632cce4ee2b3724ae
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-01-07 16:38:45 +0000

    sf-122: added navbar tests

--- frontend/jest/__tests__/navbar.js
+++ frontend/jest/__tests__/navbar.js
@@ -5,22 +5,40 @@ import { mount } from 'enzyme'
 import { navbarQuery } from '../__mocks__/navbar-query'
 
 describe ("Navbar tests", () => {
-
+    
     let navbar
+    const topLevelItems = () => navbar.findWhere(node => node.is(".navbar-item") && node.parent().is(".navbar-end"))
+    const topLinks = () => navbar.find(".navbar-link")
+    const dropdowns = () => navbar.find(".navbar-dropdown")
+    const dropLinks = () => navbar.find(".dropdown-item")
+    
     beforeEach(() => {
         navbar = mount(<NavBarDisplay links={getNavBarLinksFromGraphqlData(navbarQuery)} />)
     })
-
-    test("Dropdown menus closed on loading", () => {
-        const dropdowns = navbar.find(".navbar-dropdown")
-        const areVisible = dropdowns.map((node) => node.hasClass("is-hidden-touch"))
-        expect(areVisible).not.toContain(false)
+    
+    test("Dropdown menus have is-hidden-touch property by default", () => {        
+        const areHidden = dropdowns().map(drop => drop.is(".is-hidden-touch"))
+        expect(areHidden).not.toContain(false)
     })
 
-    test("Dropdown menus open when clicked", async () => {
-        const dropButton = navbar.find(".navbar-item.has-dropdown").at(0)
-        const dropdown = navbar.find(".navbar-dropdown").at(0)
-        dropButton.simulate("click")
-        await expect(dropdown.hasClass("is-hidden-touch")).toBe(false)
+    test("Dropdown menu classes change on click", () => {
+        topLinks().at(0).simulate("click")
+        const dropdown = dropdowns().at(0)
+        expect(dropdown.is(".is-hidden-touch")).toBe(false)
+    })
+
+    test("Dropdown menus are populated with correct link names", () => {
+        const linkNames = dropdowns().at(0).children().map(node => node.text())
+        expect(linkNames).toStrictEqual(["About Item", "Flying Pig Stage", "Full width content page", "Hatters Cafe", "Hatters Cafe"])
+    })
+
+    test("Dropdown links render correctly", () => {
+        const link = dropLinks().at(0)
+        expect(link.html()).toBe('<a class="dropdown-item" href="/about/about-item/">About Item</a>')
+    })
+
+    test("Navbar headings without subsections do not render a dropdown", () => {
+        const news = topLevelItems().at(2)
+        expect(news.children().find("navbar-dropdown")).toHaveLength(0)
     })
 })
