7183ec35630fc38b3904c3f3744856d769f00ed1
Tim Leach <Tim.Leach@softwire.com>
2020-01-07 15:49:11 +0000

    SF-39 Began work on DateTimeRange widget

---
+++ frontend/src/cms/DateTimeRangeWidget.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import CMS from 'netlify-cms-app'
+import { Map } from 'immutable'
+import { queryObjectChild } from './queryNestedWidgets'
+import PropTypes from 'prop-types'
+
+const ObjectControl = CMS.getWidget("object").control
+
+export class DateTimeRangeControl extends React.Component {  // Must use a class, as widgets don't (currently) support React hooks
+  render() {
+    const fields = new Map({
+      fields: [
+        new Map({
+          label: "Event start time",
+          name: "startDateTime",
+          widget: "datetime",
+          hint: "The date and time at which this event is due to start."
+        }),
+        new Map({
+          label: "Provide end time",
+          name: "provideEnd",
+          widget: "boolean",
+          hint: "Tick this box if you want to set an end time for this event."
+        })
+      ]
+    })
+    
+    return <ObjectControl {...this.props} onChange={value => this.handleChange(value)} field={this.getFields()} />
+  }
+
+  handleChange(value) {
+    this.props.value = value
+    this.props.field = this.getFields()
+  }
+
+  getFields() {
+    const fields = {
+      fields: [
+        new Map({
+          label: "Event start time",
+          name: "startDateTime",
+          widget: "datetime",
+          hint: "The date and time at which this event is due to start."
+        }),
+        new Map({
+          label: "Provide end time",
+          name: "provideEnd",
+          widget: "boolean",
+          hint: "Tick this box if you want to set an end time for this event."
+        })
+      ]
+    }
+
+    /*
+    // Do they want to provide an end time?
+    if (queryObjectChild(this.props.value, "provideEnd")) {
+      fields.fields.push(new Map({
+        label: "Event end time",
+        name: "endDateTime",
+        widget: "dateTime",
+        hint: "The date and time at which this event is due to end."
+      }))
+    }
+    */
+
+    return new Map(fields)
+  }
+}
+
+DateTimeRangeControl.propTypes = {
+  value: PropTypes.object,
+  field: PropTypes.instanceOf(Map)
+}
