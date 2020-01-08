a5b29425504ac0fdfcdf4b08c016c778e05cc8ab
Tim Leach <Tim.Leach@softwire.com>
2020-01-08 11:12:55 +0000

    SF-124 Added non-rendering tests

--- frontend/jest/__tests__/form-frame.js
+++ frontend/jest/__tests__/form-frame.js
@@ -4,9 +4,35 @@ import { shallow } from 'enzyme'
 import { FormFrame } from '../../src/components/FormFrame'
 
 describe("FormFrame", () => {
-  test("is null if not public", () => {
-    const formFrame = shallow(<FormFrame form={{isPublic: false}} />)
+  describe("doesn't render if", () => {
+    test("not public", () => {
+      const formFrame = shallow(<FormFrame form={{isPublic: false}} />)
+  
+      expect(formFrame.isEmptyRender()).toBeTruthy()
+    })
 
-    expect(formFrame.isEmptyRender()).toBeTruthy()
+    test("given a meaningless link", () => {
+      const formFrame = shallow(<FormFrame form={{isPublic: true, link: "Hello, world!"}} />)
+  
+      expect(formFrame.isEmptyRender()).toBeTruthy()
+    })
+  
+    test("given a valid, but not google forms, url", () => {
+      const formFrame = shallow(<FormFrame form={{isPublic: true, link: "https://www.google.com/"}} />)
+  
+      expect(formFrame.isEmptyRender()).toBeTruthy()
+    })
+
+    test("given an iframe with an invalid url", () => {
+      const formFrame = shallow(<FormFrame form={{isPublic: true, link: "<iframe src=\"Hello, world!\">"}} />)
+  
+      expect(formFrame.isEmptyRender()).toBeTruthy()
+    })
+
+    test("given an iframe with an valid, but not google forms, url", () => {
+      const formFrame = shallow(<FormFrame form={{isPublic: true, link: "<iframe src=\"https://www.google.com/\">"}} />)
+  
+      expect(formFrame.isEmptyRender()).toBeTruthy()
+    })
   })
 })
