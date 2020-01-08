ae9d940178c83bd96383318064a860affd9478b8
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-01-08 17:00:47 +0000

    sf-120: Analytics skeleton component added

---
+++ frontend/src/components/Analytics.jsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+
+export const Analytics = ({children}) => (
+    <React.Fragment>
+        <AnalyticsPermissionPopUp />
+        <GoogleAnalyticsInitialisation />
+        {children}
+    </React.Fragment>
+)
+
+const useAnalyticsPermissionSettings = () => {
+    const [isEnabled, setIsEnabled] = useState(localStorage.getItem("Analytics Permission"))
+
+    useEffect(() => {
+        if (isEnabled !== localStorage.getItem("Analytics Permission")) {
+            localStorage.setItem("Analytics Permission", isEnabled)
+        }
+    })
+
+    return [isEnabled, setIsEnabled]
+}
+
+const AnalyticsPermissionPopUp = () => {
+    const [isEnabled, setIsEnabled] = useAnalyticsPermissionSettings()
+
+    return null
+    // return pop up, whose buttons call setIsEnabled with "0" or "1"
+}
+
+const GoogleAnalyticsInitialisation = () => {
+    const [isEnabled] = useAnalyticsPermissionSettings()
+
+    if (isEnabled === "1") {
+        // Enable G.A.
+        return null
+    }
+    else {
+        // Perhaps we should check if GA is enabled and disable it if necessary?
+        return null
+    }
+}
