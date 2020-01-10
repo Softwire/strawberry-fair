f0710cec01b40a75c5f728dda2577debd797db7f
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-01-10 17:03:00 +0000

    sf-120: changed cookie notification to fixed bottom banner & reviewed names in Analytics.jsx

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -1,80 +1,76 @@
 import React, {useEffect, useState} from 'react'
 import ReactGA from 'react-ga'
 import PropTypes from 'prop-types'
+import OutsideClickHandler from 'react-outside-click-handler'
 
 import { PreviewContext } from '../util/context'
 
 export const Analytics = () => (
-    <React.Fragment>
-        <AnalyticsPermissionModal />
-        <GoogleAnalyticsWrapper />
-    </React.Fragment>
-)
-
-const useAnalyticsPermissionSettings = () => {
-    const [analyticsEnabled, setAnalyticsEnabled] = useState(undefined)
-
-    // Synchronises analyticsEnabled flag with localStorage
-    useEffect(() => {
-        if (analyticsEnabled === undefined) {
-            setAnalyticsEnabled(localStorage.getItem("Analytics Permission"))
-        }
-        else if (analyticsEnabled !== null) {
-            localStorage.setItem("Analytics Permission", analyticsEnabled)
-        }
-    })
-
-    return [analyticsEnabled, setAnalyticsEnabled]
-}
-
-const AnalyticsPermissionModal = () => {
-    const [analyticsEnabled, setAnalyticsEnabled] = useAnalyticsPermissionSettings()
-    const [modalActive, setModalActive] = useState(analyticsEnabled)
-
-    useEffect(() => {
-        setModalActive(analyticsEnabled === null)
-    }, [analyticsEnabled])
-
-    return (
-        <div className={`modal ${(modalActive) ? "is-active" : ""}`}>
-            <div className="modal-background"
-                 onClick={() => setModalActive(false)} />
-            <div className="modal-card">
-                <header className="modal-card-head">
-                    <p className="modal-card-title">Modal title</p>
-                </header>
-                <section className="modal-card-body">
-                    <p>
-                        Enable blah?
-                    </p>
-                </section>
-                <footer className="modal-card-foot">
-                    <button className="button is-success"
-                            onClick={() => setAnalyticsEnabled("1")}>
-                        Yes
-                    </button>
-                    <button className="button"
-                            onClick={() => setAnalyticsEnabled("0")}>
-                        Nah
-                    </button>
-                </footer>
-            </div>
-        </div>
-    )
-}
-
-const GoogleAnalyticsWrapper = () => (
     <PreviewContext.Consumer>
-        {(value) => <GoogleAnalytics isPreview={value} />}
+        {(value) => <AnalyticsFragment isPreview={value} />}
     </PreviewContext.Consumer>
 )
 
-// Analytics only runs in production mode, not build mode
-const GoogleAnalytics = ({isPreview}) => {
-    const [analyticsEnabled] = useAnalyticsPermissionSettings()
+const AnalyticsFragment = ({isPreview}) => (
+    <React.Fragment>
+        <CookieBanner isPreview={isPreview} />
+        <GoogleAnalytics isPreview={isPreview}/>
+    </React.Fragment>
+)
+
+const useCookieSettings = () => {
+    const [cookiesEnabled, setCookiesEnabled] = useState(undefined)
+
+    // Synchronises cookiesEnabled flag with localStorage
+    useEffect(() => {
+        if (cookiesEnabled === undefined) {
+            setCookiesEnabled(localStorage.getItem("Analytics Permission"))
+        }
+        else if (cookiesEnabled !== null) {
+            localStorage.setItem("Analytics Permission", cookiesEnabled)
+        }
+    })
+
+    return [cookiesEnabled, setCookiesEnabled]
+}
+
+const CookieBanner = ({isPreview}) => {
+    const [cookiesEnabled, setCookiesEnabled] = useCookieSettings()
+    const [bannerActive, setBannerActive] = useState(cookiesEnabled)
 
     useEffect(() => {
-        if (analyticsEnabled === "1" && !isPreview) {
+        setBannerActive(cookiesEnabled === null)
+    }, [cookiesEnabled])
+
+    if (bannerActive && !isPreview) {
+        return (
+            <OutsideClickHandler onOutsideClick={() => setBannerActive(false)}>
+                <div className="cookies-banner notification">
+                    <button className="delete" />
+                    Enable blah?
+                    <footer>
+                        <button className="button is-success"
+                                onClick={() => setCookiesEnabled("1")}>
+                            Yes
+                        </button>
+                        <button className="button"
+                                onClick={() => setCookiesEnabled("0")}>
+                            Nah
+                        </button>
+                    </footer>
+                </div>
+            </OutsideClickHandler>
+        )
+    }
+    return null
+}
+
+// Analytics only runs in production mode, not build mode
+const GoogleAnalytics = ({isPreview}) => {
+    const [cookiesEnabled] = useCookieSettings()
+
+    useEffect(() => {
+        if (cookiesEnabled === "1" && !isPreview) {
             if (!ReactGA.ga()) {
                 ReactGA.initialize("UA-63562931-2") // for debugging, add argument { "debug": true }
             }
@@ -85,4 +81,6 @@ const GoogleAnalytics = ({isPreview}) => {
     return null
 }
 
+AnalyticsFragment.propTypes = { isPreview: PropTypes.bool }
+CookieBanner.propTypes = { isPreview: PropTypes.bool }
 GoogleAnalytics.propTypes = { isPreview: PropTypes.bool }
