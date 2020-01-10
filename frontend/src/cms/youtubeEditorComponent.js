7a96a1280a37b43d78b6c553bd0ad499068a56d1
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-10 18:40:41 +0000

    SF-116: allow the CMS user to change the size of the videos.

--- frontend/src/cms/youtubeEditorComponent.js
+++ frontend/src/cms/youtubeEditorComponent.js
@@ -6,12 +6,19 @@ export const youtubeEditorComponent = {
     // Visible label
     label: "Youtube",
     // Fields the user need to fill out when adding an instance of the component
-    fields: [{name: 'iframe', label: 'Embed Youtube Video <>', widget: 'string'}],
+    fields: [
+      {name: 'iframe', label: 'Embed Youtube Video <>', widget: 'string', hint: 'Hint: Paste a youtube video iframe into this box. To find the iframe, follow the steps below:\n\t1.\
+       Go to the desired video on Youtube\n\t2. Click on "Share", above the red subscribe button.\n\t3. Click on "<> Embed"\n\t4. Click on "Copy" in the bottom-right corner.\n\t5.\
+        Paste into this box.\nYou should now see your video appearing on the preview.'},
+      {name: 'largeOnDesktop', label: 'Large size on desktop', widget: 'boolean', default: false, hint: 'This switch controls the size of the video on a desktop screen.\
+       Switch it on if you want to make it large. You should see the video change in size as you toggle the switch.'}
+    ],
     // Pattern to identify a block as being an instance of this component
-    pattern: /<figure\sclass="video-container\sis-marginless">(<iframe(?:\s.*)?\ssrc="https:\/\/www\.youtube\.com.*?"(?:\s.*>|>)<\/iframe>)<\/figure>/,
+    pattern: /<figure(?:\s.*?)?(\sis-large-on-desktop)?(?:\s.*?)?>(<iframe(?:\s.*?)?\ssrc="https:\/\/www\.youtube\.com.*?"(?:\s.*?)?><\/iframe>)<\/figure>/,
     // Function to extract data elements from the regexp match
     fromBlock: function(match) {
-      return {iframe: match[1]}
+      console.log(match)
+      return {iframe: match[2], largeOnDesktop: !!match[1]}
     },
     // Function to create a text block from an instance of this component
     toBlock: getSimplifiedIframe,
--- frontend/src/util/youtubeInfoExtractor.js
+++ frontend/src/util/youtubeInfoExtractor.js
@@ -1,14 +1,15 @@
 export function getVideoInfoString(iframe) {
     if(iframe) {
-        const height = iframe.match(/<iframe(?:\s.*)?\s(height=".*?")(?:\s.*>|>)/)
-        const width = iframe.match(/<iframe(?:\s.*)?\s(width=".*?")(?:\s.*>|>)/)
-        const source = iframe.match(/<iframe(?:\s.*)?\s(src="https:\/\/www\.youtube\.com.*?")(?:\s.*>|>)/)
+        const height = iframe.match(/\s(height=".*?")[\s>]/)
+        const width = iframe.match(/\s(width=".*?")[\s>]/)
+        const source = iframe.match(/\s(src="https:\/\/www\.youtube\.com.*?")[\s>]/)
         return source ? (height && width ? height[1] + ' ' + width[1] + ' ' + source[1] : source[1] ) : null
     }
     else return null
 }
 
-export function getSimplifiedIframe({iframe}) {
+export function getSimplifiedIframe({iframe, largeOnDesktop}) {
     const videoInfo = getVideoInfoString(iframe)
-    return videoInfo ? '<figure class="video-container is-marginless"><iframe ' + videoInfo + ' frameborder="0" allowfullscreen="true"></iframe></figure>' : ''
+    const extraClass = largeOnDesktop ? ' is-large-on-desktop' : '' 
+    return videoInfo ? '<figure class="video-container' + extraClass + ' is-marginless"><iframe ' + videoInfo + ' frameborder="0" allowfullscreen="true"></iframe></figure>' : ''
 }
