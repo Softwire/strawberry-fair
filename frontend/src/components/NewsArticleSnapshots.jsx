4d84e02873265ae82b15f3eb681f0a7b1d884c0d
Robert Owen <robert.owen@softwire.com>
2020-01-13 17:20:34 +0000

    SF-152: adjust styling of news overview page - Fixed image aspect ratio - Reformatted Date - Adjusted column widths - Added text excerpts

--- frontend/src/components/NewsArticleSnapshots.jsx
+++ frontend/src/components/NewsArticleSnapshots.jsx
@@ -1,20 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import _ from 'lodash'
 
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 import MainTileWithTwoStackedSideTiles from './MainTileWithTwoStackedSideTiles'
 import { newsArticleValidator } from './validators'
+import { HTMLContent } from './Content'
 
-const NewsArticleSnapshot = ({newsArticles}) => (
-  <article>
-    <Link to={newsArticles.node.fields.slug}>
-      <PreviewCompatibleImage imageInfo={newsArticles.node.frontmatter.image}/>
-      <h2 className="has-text-primary">{newsArticles.node.frontmatter.title}</h2>
-      <h3 className="has-text-primary">{newsArticles.node.frontmatter.date}</h3>
-    </Link>
+//display style of the event date
+export const displayStyle = {
+  day: "numeric",
+  month: "long",
+  year: "numeric"
+}
+
+const NewsArticleSnapshot = ({newsArticles}) => {
+  const publicationDate = new Date(newsArticles.node.frontmatter.date)
+
+  const imageInfo = _.get(
+    newsArticles.node.frontmatter.image,
+    'srcNode.childImageSharp.fixedAspect',
+    newsArticles.node.frontmatter.image
+  )
+  const imageWrapperStyle = {
+    width: "100%"
+  }
+
+  return (
+    <article className="news-preview">
+      <Link to={newsArticles.node.fields.slug}>
+        <PreviewCompatibleImage imageInfo={imageInfo} style={imageWrapperStyle} />
+        <h2 className="title is-5 news-snapshot-title">{newsArticles.node.frontmatter.title}</h2>
+      </Link>
+      <h3 className="subtitle is-7">{publicationDate.toLocaleDateString("en-GB", displayStyle)}</h3>
+      <HTMLContent content={newsArticles.node.shortExcerpt} className="news-excerpt news-excerpt-short" />
+      <HTMLContent content={newsArticles.node.longExcerpt} className="news-excerpt news-excerpt-long" />
   </article>
-)
+  )
+}
 
 NewsArticleSnapshot.propTypes = {
   newsArticles: newsArticleValidator
