60ff0eb28061310e94d8a735078abada24ddc8e3
Joel Wee <Joel.Wee@softwire.com>
2020-01-14 11:52:29 +0000

    SF-141: modify notes widget

--- frontend/src/cms/cms.js
+++ frontend/src/cms/cms.js
@@ -94,7 +94,7 @@ CMS.registerWidget("hero", HeroControl)
 CMS.registerWidget('multi-collection-relation', MultiCollectionRelationControl, MultiCollectionRelationPreview)
 CMS.registerWidget('link', LinkControl, LinkPreview)
 CMS.registerWidget("dateTimeRange", DateTimeRangeControl)
-CMS.registerWidget('notes', CMS.getWidget('markdown').control, null)
+CMS.registerWidget('notes', CMS.getWidget('markdown').control, () => null)
 
 CMS.registerEditorComponent(youtubeEditorComponent)
 
