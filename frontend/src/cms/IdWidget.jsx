fd8f0ecb3c612c85e388ad211a2f2102548757fd
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-16 09:53:33 +0000

    SF-132: adding a console log in IDwidget

--- frontend/src/cms/IdWidget.jsx
+++ frontend/src/cms/IdWidget.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export class IdControl extends React.Component {
   render() {
   console.log("I'm in the IdControl render function")
+  console.log(`Current value is ${this.props.value}`)
   console.log(this.props.value||new Date().toLocaleString())
   this.props.onChange(this.props.value||new Date().toLocaleString())
     return null
