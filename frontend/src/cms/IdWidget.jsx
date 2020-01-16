90426d5b8763e33dfc811f0e26ccf8be127eeb70
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-16 10:00:48 +0000

    SF-132: make changes on the ID only if it hasn't been defined yet.

--- frontend/src/cms/IdWidget.jsx
+++ frontend/src/cms/IdWidget.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export class IdControl extends React.Component {
   render() {
-  console.log("I'm in the IdControl render function")
-  console.log(`Current value is ${this.props.value}`)
-  console.log(this.props.value||new Date().toLocaleString())
-  this.props.onChange(this.props.value||new Date().toLocaleString())
+    console.log(`Current value is: ${this.props.value}`)
+    if(!this.props.value) {
+      this.props.onChange(new Date().toLocaleString())
+    }
+    console.log(`Value is now: ${this.props.value}`)
     return null
   }
 }
