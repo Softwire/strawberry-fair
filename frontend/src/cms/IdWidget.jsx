782d05f328ab33551150d18c08b456dc1ceadacf
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-16 09:52:40 +0000

    SF-132: set the ID to be the current time.

--- frontend/src/cms/IdWidget.jsx
+++ frontend/src/cms/IdWidget.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export class IdControl extends React.Component {
   render() {
   console.log("I'm in the IdControl render function")
-  this.props.onChange('hello')
+  console.log(this.props.value||new Date().toLocaleString())
+  this.props.onChange(this.props.value||new Date().toLocaleString())
     return null
   }
 }
