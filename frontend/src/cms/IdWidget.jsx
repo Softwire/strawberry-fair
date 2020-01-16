b3e7afb0c073a3f75f3df92c1540a5a1ef0e24f6
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-01-16 09:40:40 +0000

    SF-132: get ready to test if CMS is writing the ID

--- frontend/src/cms/IdWidget.jsx
+++ frontend/src/cms/IdWidget.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export class IdControl extends React.Component {
   render() {
+  console.log("I'm in the IdControl render function")
   this.props.onChange('hello')
     return null
   }
