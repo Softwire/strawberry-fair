478073a529123b76bfab74193424b76a126392f6
Matthew Wilson <Matthew.Wilson@softwire.com>
2020-01-23 16:44:06 +0000

    sf-186: added support for multiline titles

--- frontend/src/components/Titles.jsx
+++ frontend/src/components/Titles.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { HTMLContent } from './Content'
+
 // Subtitle is rendered as <p> rather than <h2> due to instructions at
 // https://www.w3.org/TR/html52/common-idioms-without-dedicated-elements.html#subheadings-subtitles-alternative-titles-and-taglines
 export const Titles = ({title, subtitle}) => {
   if (title || subtitle) {
     return (
       <div className="titles">
-        <h1 className="title is-2 is-size-3-mobile is-spaced">{title}</h1>
+        <h1 className="title is-2 is-size-3-mobile is-spaced">
+          <HTMLContent content={addBreaks(title)} />
+        </h1>
         <p className="subtitle is-5">{subtitle}</p>
       </div>
     )
@@ -20,3 +24,5 @@ Titles.propTypes = {
   title: PropTypes.node,
   subtitle: PropTypes.node
 }
+
+const addBreaks = (title) => title.replace(/\. /g, ".<br>")
